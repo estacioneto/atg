924490ed9dbd1b20eb52bb483642f5b5bc8f311b
Eric Breno <ebreno02@gmail.com>
2018-04-25 15:29:10 -0300

    Criados métodos para leitura de grafo e modelo inicial

---
+++ src/main/java/manager/GraphManager.java
@@ -0,0 +1,88 @@
+package manager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import model.Graph;
+
+public class GraphManager implements GraphManagerInterface {
+
+	private Scanner getScanner(String path) {
+		File file = new File(path);
+		try {
+			return new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public Graph readGraph(String path) {
+		Scanner sc = getScanner(path);
+		int edges = sc.nextInt();
+		Graph graph = new Graph();
+		while (sc.hasNext()) {
+			String line = sc.nextLine();
+			graph.connect(line);
+		}
+		return graph;
+	}
+
+	public Graph readWeightedGraph(String path) {
+		Scanner sc = getScanner(path);
+		int edges = sc.nextInt();
+		Graph graph = new Graph();
+		while (sc.hasNext()) {
+			String line = sc.nextLine();
+			graph.connectWeighted(line);
+		}
+		return graph;
+	}
+
+	public int getVertexNumber(Graph graph) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getEdgeNumber(Graph graph) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public float getMeanEdge(Graph graph) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String graphRepresentation(Graph graph, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String BFS(Graph graph, int v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String DFS(Graph graph, int v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String SCC(Graph graph) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String shortestPath(int v1, int v2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String mst(Graph graph) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/model/Graph.java
+++ src/main/java/model/Graph.java
@@ -1,4 +1,76 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Graph {
+
+	private Map<Node, List<Node>> nodes;
+
+	public Graph() {
+		this.nodes = new HashMap<Node, List<Node>>();
+	}
+
+	private void checkConnect(Node nodeA, Node nodeB) {
+		if (!this.nodes.containsKey(nodeA)) {
+			this.nodes.put(nodeA, new ArrayList<Node>());
+		}
+		if (!this.nodes.containsKey(nodeB)) {
+			this.nodes.put(nodeB, new ArrayList<Node>());
+		}
+		this.nodes.get(nodeA).add(nodeB);
+		this.nodes.get(nodeB).add(nodeA);
+	}
+
+	public void connect(String pair) {
+		String[] nodes = pair.split(" ");
+		Node nodeA = new Node(nodes[0]);
+		Node nodeB = new Node(nodes[1]);
+		checkConnect(nodeA, nodeB);
+	}
+
+	public void connectWeighted(String triple) {
+		String[] nodes = triple.split(" ");
+		Node nodeA = new Node(nodes[0], nodes[2]);
+		Node nodeB = new Node(nodes[1], nodes[2]);
+		checkConnect(nodeA, nodeB);
+	}
+
+	private class Node {
+		int weight;
+		int value;
+
+		Node(String value) {
+			this.value = Integer.parseInt(value);
+		}
+
+		Node(String value, String weight) {
+			this(value);
+			this.weight = Integer.parseInt(weight);
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + value;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Node other = (Node) obj;
+			if (value != other.value)
+				return false;
+			return true;
+		}
+	}
 }
