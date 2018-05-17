b92ebd5c90a07408f7bc25efde6646816f1aa770
Luciano de Oliveira Júnior <luciannojunior@gmail.com>
2018-05-17 09:47:43 -0300

    Adicionando arquitetura de testes

--- src/main/java/algorithms/BreadthSearch.java
+++ src/main/java/algorithms/BreadthSearch.java
@@ -1,10 +1,10 @@
 package algorithms;
 
-import javafx.util.Pair;
 import model.Graph;
 
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.io.Serializable;
 
 public class BreadthSearch {
     public static String BFS(Graph graph, int init) {
@@ -29,5 +29,33 @@ public class BreadthSearch {
         }
 
         return sb.toString();
+    }
+
+    static class Pair<K,V> implements Serializable{
+
+        private K key;
+
+        public K getKey() { return key; }
+
+        private V value;
+
+        public V getValue() { return value; }
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o instanceof Pair) {
+                Pair pair = (Pair) o;
+                if (key != null ? !key.equals(pair.key) : pair.key != null) return false;
+                if (value != null ? !value.equals(pair.value) : pair.value != null) return false;
+                return true;
+            }
+            return false;
+        }
     }
 }
---
+++ src/test/java/GraphTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class GraphTest {
+
+    @Test
+    void testUmMaisUm() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
